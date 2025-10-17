68b064eeff84773ad93bc7ae9ca2720cdb6ed82c
v9filipe <Filipe.silva.ipt@icloud.com>
2025-10-17 07:20:18 +0100

    Update comment for base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hotel-pms/', // Ensure this matches your GitHub repository name
+  base: '/hotel-pms/', // MUST match your repo name
 });
